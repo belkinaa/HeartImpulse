bdbbf47505369d4a4715d6f1c1afb4fbe7ccd3f1
belkin aleksej <belkin.a.a@inbox.ru>
2023-08-24 03:06:18 +0500

    Конфигурация для запуска на OpenServer

--- heart_imp_app/project_setting.py
+++ heart_imp_app/project_setting.py
@@ -1,7 +1,4 @@
-import os
-POSTGRES_USER = os.environ['USER_FOR_DOCKER']
-POSTGRES_PASSWORD = os.environ['PASSWORD_FOR_DOCKER']
 class Settings_app():
     def __init__(self):
         self.domenServer = 'http://127.0.0.1:5000/'
-        self.app_db_connect = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/db_docker'
+        self.app_db_connect = f'postgresql://postgres:@127.0.0.1:5432/HI_db'
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from heart_imp_app import HI_App as app
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
