639bc89988e9d1c6fc0198bc909f89b464ee9017
belkin aleksej <belkin.a.a@inbox.ru>
2023-08-24 01:29:56 +0500

    rename for Docker-compose

--- heart_imp_app/project_setting.py
+++ heart_imp_app/project_setting.py
@@ -1,4 +1,4 @@
 class Settings_app():
     def __init__(self):
         self.domenServer = 'http://127.0.0.1:5000/'
-        self.app_db_connect = 'postgresql://postgres:@127.0.0.1:5432/HI_db'
+        self.app_db_connect = 'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/db_docker'
